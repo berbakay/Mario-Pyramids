621dac78bd903792223b61f0a0584a6ef224df59
berbakay <rick.kay90@gmail.com>
2020-11-08 12:44:32 +0000

    add comments

--- mario.c
+++ mario.c
@@ -3,28 +3,28 @@
 
 int main() 
 {
-    int height = get_int("height: ");
+    int height = get_int("height: "); //get height from user 
 
-    while (height > 8 || height < 1) 
+    while (height > 8 || height < 1) //test if height is between 1 and 8, if not ask for height again.
     {
         height = get_int("height: ");
     }
 
-    for (int i = 0; i < height; i++)
+    for (int i = 0; i < height; i++) //loop number of times given by user
     {
-        for (int j = i + 1; j < height; j++)
+        for (int j = i + 1; j < height; j++) //print number of spaces at start of line 
         {
             printf(" ");
         }
 
-        for (int k = i + 1; k > 0; k--)
+        for (int k = i + 1; k > 0; k--) //print first set of hashes in line
         {
             printf("#");
         }
 
-        printf("  ");
+        printf("  "); //print 2 center spaces
 
-        for (int j = i + 1; j > 0; j--) 
+        for (int j = i + 1; j > 0; j--) //print final hashes
         {
             printf("#");
         }
